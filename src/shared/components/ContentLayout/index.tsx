f7ff553fa5edb5cf4638f3db0d197db56182e4cf
william <hr.g5studio@gmail.com>
2023-05-17 15:19:56 +0800

    feat: 切換頁面後自動捲動至頂

--- src/shared/components/ContentLayout/index.tsx
+++ src/shared/components/ContentLayout/index.tsx
@@ -1,14 +1,18 @@
 import { IBaseComponentProp } from "@shared/interfaces/base-component.interface";
 import "./style.scss";
+import { useEffect } from "react";
 
 interface Props extends IBaseComponentProp {
 
 }
 
-const ContentLayout = ({ children, testId, classes }: Props) =>
-    <section data-testid={testId} className={`component-content-layout ${classes ?? ''}`}>
+const ContentLayout = ({ children, testId, classes }: Props) => {
+    useEffect(() => window.scrollTo({ top: 0, behavior: 'smooth' }), []);
+    return <section data-testid={testId} className={`component-content-layout ${classes ?? ''}`}>
         {children}
     </section>;
+}
+
 
 
 export default ContentLayout;
