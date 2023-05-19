15dea60b1054d016aa02b319ddddc458a45df987
william <hr.g5studio@gmail.com>
2023-05-19 17:48:22 +0800

    fix: 修正關於我們內文未分段錯誤

--- src/modules/main/pages/IntroductionPage/index.tsx
+++ src/modules/main/pages/IntroductionPage/index.tsx
@@ -2,7 +2,7 @@ import ContentLayout from '@shared/components/ContentLayout';
 import banner from 'assets/img/about-us/banner.svg';
 import bannerMobile from 'assets/img/about-us/banner-mobile.svg';
 
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 import './style.scss';
 
@@ -13,7 +13,13 @@ const IntroductionPage = () => {
         <img className='d-block d-sm-none w-100' src={bannerMobile} alt="" />
         <section className='page-introduction__container pt-12 pt-sm-20 mx-auto pb-21_5 pb-sm-30 pb-lg-50 px-5 px-sm-0'>
             <h2 className='text-black-1 text-center'>{t('pages.about.title')}</h2>
-            <p className='font-xs text-gray-2 font-sm-lg mt-7 mt-sm-10 mt-lg-15'>{t('pages.about.content')}</p>
+            <Trans
+                i18nKey={'pages.about.content'}
+                components={{
+                    1: <p className='font-xs text-gray-2 font-sm-lg mt-7 mt-sm-10 mt-lg-15' />,
+                    2: <p className='font-xs text-gray-2 font-sm-lg mt-5 mt-sm-7' />
+                }}
+            />
         </section>
     </ContentLayout>
 }
