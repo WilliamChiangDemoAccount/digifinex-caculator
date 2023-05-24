b36006fcfcf5057377f249add48233faf9ef7c31
Stella Lu <stella.lu@psgp.com>
2023-05-24 18:27:21 +0800

    fix. update announcementDate

--- src/modules/announcement/constants/announcement-config.ts
+++ src/modules/announcement/constants/announcement-config.ts
@@ -16,16 +16,16 @@ export const announcementConfig: Map<Announcement, AnnouncementConfig> = new Map
     [
         Announcement.PrivacyPolicy,
         {
-            announcementDate: '2023-01-01',
-            effectiveDate: '2023-02-01',
+            announcementDate: '2023-05-19',
+            effectiveDate: '2023-05-19',
             html: 'Privacy Policy'
         }
     ],
     [
         Announcement.TermsofUse,
         {
-            announcementDate: '2023-01-01',
-            effectiveDate: '2023-02-01',
+            announcementDate: '2023-05-19',
+            effectiveDate: '2023-05-19',
             html: 'Terms of Use'
         }
     ],
